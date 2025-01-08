a8ec91e6b1e8904376e5b489a7e486c764700bf4
marco torremacco <mtorremacco@gmail.comm>
2025-01-08 16:05:37 +0100

    finish project

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function generateMail () {
-    const mails = []   
+//DOM ELMS
+const emailListElm = document.getElementById('email-list')
+const generateBtnElm = document.getElementById('generate-btn')
 
+//functions
+function generateMail() {
+
+    emailListElm.innerHTML = '';
+   
    for (let i = 0; i < 10; i++){
-        axios.get('https:flynn.boolean.careers/exercises/api/random/mail')
+    axios.get('https:flynn.boolean.careers/exercises/api/random/mail')
     .then(function (response) {
-        mails.push(response.data.response)
+        emailListElm.innerHTML += `<li>${response.data.response}</li>`
     })
+    }
 }
-}
+
+generateMail();
+
+//events
+generateBtnElm.addEventListener('click', generateMail);
