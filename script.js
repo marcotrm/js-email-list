31ac33061222d7210ab7aaea8b709be5345bccf8
marco torremacco <mtorremacco@gmail.comm>
2025-01-08 19:28:03 +0100

    feat: add loading on loading and the mails appear together

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ const generateBtnElm = document.getElementById('generate-btn')
 
 //functions
 function generateMail() {
-
-    emailListElm.innerHTML = '';
+    emailListElm.innerHTML = '<li>Loading...</li>';
+    const randomMails = []
    
    for (let i = 0; i < 10; i++){
     axios.get('https:flynn.boolean.careers/exercises/api/random/mail')
     .then(function (response) {
-        emailListElm.innerHTML += `<li>${response.data.response}</li>`
-    })
+       randomMails.push(`<li>${response.data.response}</li>`)
+
+       if (randomMails.length === 10) {
+        emailListElm.innerHTML = randomMails.join('');
+                }   
+        } )
     }
 }
 
